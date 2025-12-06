90e439e48264b44c2f62b6975d36f2ad809814f0
Dallas <dcszittai@gmail.com>
2025-12-05 23:01:38 -0500

    Fixed Github Pages Dist

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'docs', // Change 'dist' to 'docs'
+    outDir: 'docs',
   },
 });
